2fad7d60c3a68abc21e4b13410af212fa193e93c
AndreyDuda <andreyduda88@gmail.com>
2019-01-23 21:31:56 +0200

    settings project complate

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .setPublicPath('public/build')
+    .setResourceRoot('build')
+    .js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/bootstrap.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .version();
+
